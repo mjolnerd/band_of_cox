c4845419e645404893ef4b0700a212e4e2fb9d09
Matt Shultz <mshultz@myrtle.megan.mjolnerd.com>
2017-09-24 18:42:22 -0700

    That got me the page with the data. Time to drill.

--- band_of_cox.py
+++ band_of_cox.py
@@ -4,29 +4,39 @@ Shamelessly stolen from https://gist.github.com/yosemitebandit/1805918
 Going to use this as a base for scraping current Bandwidth usage from
 cox.net.
 '''
+import sys
 import requests
 from BeautifulSoup import BeautifulSoup
 
 # need to capture a valid csrf token
 # first visit the login page to generate one
 s = requests.session()
-response = s.get('https://callmeduele.com/login')
-
+response = s.get('https://www.cox.com/resaccount/sign-in.cox')
+'''
 # extract the token
 soup = BeautifulSoup(response.text)
 for n in soup('input'):
     if n['name'] == '_csrf_token':
         token = n['value']
         break
-
+'''
+try:
+    from creds import *
+except ImportError:
+    sys.exit()
 # now post to that login page with some valid credentials and the token
 auth = {
-    'userName': 'batman'
-    , 'password': 'j0kersuck5'
-    , '_csrf_token': token
+    'targetFN': 'COX.net'
+    ,'emaildomain': '@cox.net'
+    ,'username': coxuser
+    ,'password': coxpass
+    ,'signin-submit': 'Sign In'
+    ,'onsuccess': 'https%3A%2F%2Fwww.cox.com%2Fresaccount%2Fhome.cox'
+    ,'onfailure': 'http://www.cox.com/resaccount/sign-in.cox?onsuccess=https%3A%2F%2Fwww.cox.com%2Fresaccount%2Fhome.cox'
 }
-s.post('https://callmeduele.com/login', data=auth)
+print auth
+s.post('https://idm.east.cox.net/idm/coxnetlogin', data=auth)
 
 # now we should be authenticated, try visiting a protected page
-response = s.get('https://callmeduele.com/cases')
+response = s.get('https://www.cox.com/internet/mydatausage.cox')
 print response.text
